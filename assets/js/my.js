3a0f299033b2f8ed309410df0207d025ce34fc58
0pFox <4lbr3ch7@gmail.com>
2019-06-09 05:51:39 -0700

    some js menu links

--- assets/js/my.js
+++ assets/js/my.js
@@ -1,8 +1,18 @@
-$("#bgm-btn").click(function() {
-  $("#brg-menu").addClass("menu-open");
-});
 
-  function menuswitch() {
-    var element = document.getElementById("#brg-menu");
-    element.classList.add("menu-open");
-  }
+$('document').ready(function(){
+    $("#bgm-btn").click(function() {
+      $("#brg-menu").removeClass("menu-close");
+      $("#brg-menu").addClass("menu-open");
+      $(".fas").addClass("closeslide");
+      $(".bgm-txt").addClass("textslide");
+    });
+    $(".bgm-close").click(function(){
+      $("#brg-menu").addClass("menu-close");
+      $(".fas").removeClass("closeslide");
+      $(".bgm-txt").removeClass("textslide");
+    });
+    function menuswitch() {
+      var element = document.getElementById("#brg-menu");
+      element.classList.add("menu-open");
+    }
+});
