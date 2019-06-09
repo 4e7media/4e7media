a7ce59c008c2b94c002bd6aaab99239ddb14ebe4
0pFox <4lbr3ch7@gmail.com>
2019-06-09 07:27:50 -0700

    menu js and style

--- assets/js/my.js
+++ assets/js/my.js
@@ -5,8 +5,32 @@ $('document').ready(function(){
       $("#brg-menu").addClass("menu-open");
       $(".fas").addClass("closeslide");
       $(".bgm-txt").addClass("textslide");
+      $(".wrapper").addClass("click-tr");
     });
     $(".bgm-close").click(function(){
+      $("#brg-menu").addClass("menu-close");
+      $(".fas").removeClass("closeslide");
+      $(".bgm-txt").removeClass("textslide");
+      $(".wrapper").removeClass("click-tr");
+    });
+    jQuery(function($) {
+
+      var $nav = $('#bgm-btn');
+      var $win = $(window);
+      var winH = $win.height();   // Get the window height.
+  
+      $win.on("scroll", function () {
+          if ($(this).scrollTop() > winH ) {
+              $nav.addClass("show");
+          } else {
+              $nav.removeClass("show");
+          }
+      }).on("resize", function(){ // If the user resizes the window
+         winH = $(this).height(); // you'll need the new height value
+      });
+  
+  });
+    $(".bgm-text").click(function(){
       $("#brg-menu").addClass("menu-close");
       $(".fas").removeClass("closeslide");
       $(".bgm-txt").removeClass("textslide");
