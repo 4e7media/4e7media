973dcdddecaade096b4480569222beaa4b8765cb
0pFox <4lbr3ch7@gmail.com>
2019-06-10 13:58:11 -0700

    back

--- assets/js/my.js
+++ assets/js/my.js
@@ -62,7 +62,15 @@ $('document').ready(function(){
       }, 100)
       console.log(window.screen.width)
     });
-    
+    var domain = "https://" + window.location.hostname;
+
+    function close_view(){
+      if(document.referrer == domain + "/index.html"){
+        history.back();
+      }else{
+        location.href= domain + "/index.html";
+      }
+    }
     jQuery(function($) {
 
       var $nav = $('#bgm-btn');
