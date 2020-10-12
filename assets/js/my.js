b303e8291446690bcbc019243745b2b86d396943
4e7media <pa@4e7media.com>
2020-10-12 09:24:50 +0200

    under construct

--- assets/js/my.js
+++ assets/js/my.js
@@ -638,8 +638,6 @@ $(window).on("load", function () {
 var $wp = $(window).width() * 0.7
 var $hp = $(window).height()
 $(window).on('load', function () {
-  $(".wrapper").css("visibility", "visible")
-  $(".load").hide()
   if ($hp >= $wp) {
     $('.bgvideo').css({
       'height': '100vh',
